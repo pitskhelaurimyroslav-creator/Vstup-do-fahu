d47c4c14d2f8630f9b871067744b3eb173a4bdcd
pitskhelaurimyroslav-creator <pitskhelaurimyroslav@gmail.com>
2026-02-07 17:16:31 +0200

    Add student survey script in main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+print("Анкета студента")
+print("----------------")
+
+name = "Піцхелаурі Мирослав"
+age = 16
+status = "Студент"
+group = "КІ-25"
+city = "Київ"
+future_job = "Працювати в IT-компанії"
+
+print("Ім'я:", name)
+print("Вік:", age)
+print("Статус:", status)
+print("Група:", group)
+print("Місто проживання:", city)
+print("Плани на майбутнє:", future_job)
