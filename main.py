843bca8634c1ea8e3bd92f10381166639c6cdf2e
Мирослав Піцхелаурі <pitskhelaurimyroslav@gmail.com>
2026-02-12 23:26:21 +0200

    Add additional student information to main.py

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 print("Анкета студента")
-print("----------------")
 
 name = "Піцхелаурі Мирослав"
 age = 16
 status = "Студент"
 group = "КІ-25"
 city = "Київ"
+country = "Україна"
+height = 179 "см"
+weight = 70 "кг"
+study = True
 future_job = "Працювати в IT-компанії"
 
 print("Ім'я:", name)
@@ -13,4 +16,7 @@ print("Вік:", age)
 print("Статус:", status)
 print("Група:", group)
 print("Місто проживання:", city)
+print("Країна:", country)
+print("3piст:", height, "см")
+print("Вага", weight, "кг")
 print("Плани на майбутнє:", future_job)
